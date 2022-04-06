44f2cb79d2b35a1ecc6731943d69c4dd89bb26eb
a101201031 <a101201031@gmail.com>
2022-04-06 23:49:25 +0900

    feat: divide type

--- packages/frontend/src/components/sign/SignIn.tsx
+++ packages/frontend/src/components/sign/SignIn.tsx
@@ -10,11 +10,7 @@ import Paper from '@mui/material/Paper';
 import Box from '@mui/material/Box';
 import Grid from '@mui/material/Grid';
 import Typography from '@mui/material/Typography';
-
-interface SignInInputTypes {
-  email: string;
-  password: string;
-}
+import { SignInInputTypes } from 'model';
 
 function SignIn() {
   const { control, handleSubmit } = useForm<SignInInputTypes>();
---
+++ packages/frontend/src/model/sign.ts
@@ -0,0 +1,12 @@
+export interface SignInInputTypes {
+  email: string;
+  password: string;
+}
+
+export interface SignUpInputTypes {
+  name: string;
+  email: string;
+  password: string;
+  confirmPassword: string;
+  infoAgreement: boolean;
+}
