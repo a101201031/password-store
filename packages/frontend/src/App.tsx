0d9ecbae0c7ddf2dbe20c55916156a8234f9a375
a101201031 <a101201031@gmail.com>
2022-04-13 23:56:53 +0900

    feat: AccountCard testcall page

--- packages/frontend/src/App.tsx
+++ packages/frontend/src/App.tsx
@@ -1,9 +1,15 @@
 import { Routes, Route, Link } from 'react-router-dom';
 import { SignUp, SignIn } from 'components/sign';
+import { Main } from 'components/Main';
+import { AccountList, ShowPassword } from 'components';
 
 export const App = () => (
   <Routes>
-    <Route path="/" element={<SignIn />} />
+    <Route path="/" element={<Main />}>
+      <Route path="/main" element={<ShowPassword />} />
+      <Route path="/account-list" element={<AccountList />} />
+    </Route>
+    <Route path="/signin" element={<SignIn />} />
     <Route path="/signup" element={<SignUp />} />
   </Routes>
 );
---
+++ packages/frontend/src/components/index.ts
@@ -0,0 +1,2 @@
+export * from './ShowPassword';
+export * from './AccountList';
