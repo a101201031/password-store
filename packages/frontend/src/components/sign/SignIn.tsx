f34d708dd069acf7a32dafd53cf1a966eea384b2
a101201031 <a101201031@gmail.com>
2023-05-20 22:19:00 +0900

    fix: fix unsplash url

--- packages/frontend/src/components/sign/SignIn.tsx
+++ packages/frontend/src/components/sign/SignIn.tsx
@@ -101,7 +101,7 @@ function SignIn() {
         sm={4}
         md={7}
         sx={{
-          backgroundImage: 'url(https://source.unsplash.com/random)',
+          backgroundImage: 'url(https://source.unsplash.com/random?wallpapers)',
           backgroundRepeat: 'no-repeat',
           backgroundColor: (t) =>
             t.palette.mode === 'light'
